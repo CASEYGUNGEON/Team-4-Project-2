d3a9cc086a988fb0c7ede0c9b449d9c057aee6f7
IsaiahLee667 <40584744+IsaiahLee667@users.noreply.github.com>
2022-05-10 16:04:57 -0400

    Refactored Test Folder, added Test class for Services

---
+++ src/test/java/dev/group4/ServiceTests/PotLuckSerivceTests.java
@@ -0,0 +1,12 @@
+package dev.group4.ServiceTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PotLuckSerivceTests {
+
+    @Test
+    void registerUser() {
+    }
+}
