36e3e839d4bd7ea73b9a3ab0dd69df3d65563c89
IsaiahLee667 <40584744+IsaiahLee667@users.noreply.github.com>
2022-05-10 11:46:42 -0400

    Added Test statement in userserviceimpl

--- src/main/java/dev/group4/services/UserServiceImpl.java
+++ src/main/java/dev/group4/services/UserServiceImpl.java
@@ -5,6 +5,8 @@ import dev.group4.entities.User;
 public class UserServiceImpl implements UserService{
     @Override
     public User registerUser(User user) {
+        System.out.println("Adding a sout to registerUser in the UserServiceImpl to test pull");
+
         return null;
     }
 
