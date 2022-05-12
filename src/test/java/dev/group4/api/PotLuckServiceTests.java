3a44d4109b2d3392687786ca9edca1559fbc9de1
dmbrumagin <dbrumagin@gmail.com>
2022-05-11 22:59:45 -0400

    potluck service tests

--- src/test/java/dev/group4/api/PotLuckServiceTests.java
+++ src/test/java/dev/group4/api/PotLuckServiceTests.java
@@ -1,5 +1,6 @@
 package dev.group4.api;
 
+import dev.group4.aspects.InvalidTimeException;
 import dev.group4.entities.Potluck;
 import dev.group4.repos.PotluckRepo;
 import dev.group4.services.PotluckServiceImpl;
@@ -16,45 +17,48 @@ public class PotLuckServiceTests {
     static Potluck testPotluck = null;
 
     @Autowired
-
     private PotluckRepo potluckRepo;
-    PotluckServiceImpl potluckService = new PotluckServiceImpl();
-   // Potluck potluck = new Potluck("First", 0L, "username",true);
-    //Potluck potluck = new Potluck("", 2000L, "aaaa",true);
+    @Autowired
+    private PotluckServiceImpl potluckService;
 
     @Test
     @Order(1)
-    void schedulePotluckTest() {
+    void schedulePotluckTest() throws InvalidTimeException {
         Potluck newPotLuck = new Potluck("First", System.currentTimeMillis() + 1000L, "username",true);
-        //Potluck potluck = new Potluck("First", 0L, "username",true);
-        PotLuckServiceTests.testPotluck = newPotLuck;
-        potluckService.schedulePotluck(newPotLuck);
+        testPotluck =potluckService.schedulePotluck(newPotLuck);
         Assertions.assertNotEquals("", newPotLuck.getId());
     }
 
     @Test
     @Order(2)
-    void getAllPotlucksTest(){
-        Potluck test1 = new Potluck("Second", 0L, "username", true);
-        Potluck test2 = new Potluck("Third", 0L, "username", true);
-        Potluck test3 = new Potluck("Fourth", 0L, "username", true);
+    void getAllPotlucksTest() throws InvalidTimeException {
+        Potluck test1 = new Potluck("Second", System.currentTimeMillis() +10L, "username", true);
+        Potluck test2 = new Potluck("Third", System.currentTimeMillis() +3600012L, "username", true);
+        Potluck test3 = new Potluck("Fourth",System.currentTimeMillis() + 7200013L, "username", false);
 
         potluckService.schedulePotluck(test1);
         potluckService.schedulePotluck(test2);
         potluckService.schedulePotluck(test3);
 
-        List<Potluck> list1 = potluckService.getAllPotlucks();
-        System.out.println(list1);
+        List<Potluck> list1 = potluckService.getAllPublicPotlucks();
+        Assertions.assertEquals(3,list1.size()); //includes first visible from test before
 
     }
 
 
     @Test
     @Order(3)
-    void changeTimeTest(){
-        Potluck retrievedPotLuck = testPotluck;
-        potluckService.changePotluckTime(retrievedPotLuck, System.currentTimeMillis() + 3000L);
-        Assertions.assertEquals(3000L, retrievedPotLuck.getDateTime());
+    void changeTimeTest() throws InvalidTimeException {
+        long test = testPotluck.getDateTime();
+        Potluck retrievedPotLuck = potluckService.changePotluckTime(testPotluck, System.currentTimeMillis() + 3604000L);
+        Assertions.assertNotEquals(test, retrievedPotLuck.getDateTime());
+    }
+
+    @Test
+    @Order(4)
+    void cancelTest() {
+        System.out.println(testPotluck);
+        Assertions.assertTrue(potluckService.cancelPotluck(testPotluck));
     }
 //    @Test
 //    @Order(3)
