bfe6a542b3a2b82531afbe1280978afa925e6bec
David Brumagin <dbrumagin@gmail.com>
2022-05-13 15:40:43 -0400

    error/logging

--- src/main/java/dev/group4/controllers/PotluckController.java
+++ src/main/java/dev/group4/controllers/PotluckController.java
@@ -2,10 +2,12 @@ package dev.group4.controllers;
 
 
 import dev.group4.aspects.InvalidTimeException;
-import dev.group4.aspects.Secured;
+import dev.group4.aspects.LoggingAspect;
 import dev.group4.entities.Potluck;
 import dev.group4.services.PotluckService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
@@ -56,4 +58,13 @@ public class PotluckController {
         return potluckService.getPotluckById(potluck_id);
     }
 
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Object handleInvalidTimeException(Throwable e){
+        LoggingAspect.LogError(e);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage().substring(e.getMessage().indexOf(":")+1));
+    }
 }
+
