4a0284a30097884d836acccd4d6fb5ac2a7c6488
bmokua <brianmmokua@gmail.com>
2022-05-13 17:59:58 -0400

    Revert "Included is update to PotluckServiceImpl and PotLuckServiceTests"

--- src/test/java/dev/group4/api/PotLuckServiceTests.java
+++ src/test/java/dev/group4/api/PotLuckServiceTests.java
@@ -1,6 +1,5 @@
 package dev.group4.api;
 
-import dev.group4.aspects.InvalidCredentialException;
 import dev.group4.aspects.InvalidTimeException;
 import dev.group4.entities.Potluck;
 import dev.group4.repos.PotluckRepo;
@@ -16,8 +15,7 @@ import java.util.List;
 public class PotLuckServiceTests {
 
     static Potluck testPotluck = null;
-    static Potluck testPotluck1 = null;
-    static List<Potluck> list1 = null;
+
     @Autowired
     private PotluckRepo potluckRepo;
     @Autowired
@@ -33,23 +31,24 @@ public class PotLuckServiceTests {
 
     @Test
     @Order(2)
-    void getPotluckByIdTest(){
-        Potluck retrievedPotluck = potluckService.getPotluckById(testPotluck.getId());
-        System.out.println(retrievedPotluck);
-        Assertions.assertEquals(testPotluck.getId(), retrievedPotluck.getId());
+    void getAllPotlucksTest() throws InvalidTimeException {
+
+        Potluck test1 = new Potluck("Second", System.currentTimeMillis() +10L, "username", true);
+        Potluck test2 = new Potluck("Third", System.currentTimeMillis() +3600012L, "username", true);
+        Potluck test3 = new Potluck("Fourth",System.currentTimeMillis() + 7200013L, "username", false);
+
+        potluckService.schedulePotluck(test1);
+        potluckService.schedulePotluck(test2);
+        potluckService.schedulePotluck(test3);
+
+        List<Potluck> list1 = potluckService.getAllPublicPotlucks();
+        Assertions.assertEquals(3,list1.size()); //includes first visible from test before
+
     }
+
 
     @Test
     @Order(3)
-    void getAllPotlucksTest() throws InvalidTimeException {
-
-        list1 = potluckService.getAllPublicPotlucks();
-        System.out.println(list1);
-        Assertions.assertTrue(list1 != null && !list1.isEmpty());
-    }
-
-    @Test
-    @Order(5)
     void changeTimeTest() throws InvalidTimeException {
         long test = testPotluck.getDateTime();
         testPotluck.setDateTime( System.currentTimeMillis() + 3604000L);
@@ -58,55 +57,9 @@ public class PotLuckServiceTests {
     }
 
     @Test
-    @Order(6)
+    @Order(4)
     void cancelTest() {
         System.out.println(testPotluck);
         Assertions.assertTrue(potluckService.cancelPotluck(testPotluck));
     }
-
-    //negative tests
-
-    @Test
-    @Order(7)
-    void registerPotluckForNegativeTest() throws InvalidTimeException {
-        Potluck newPotLuck = new Potluck("First", System.currentTimeMillis() + 500L, "username",true);
-        testPotluck1 =potluckService.schedulePotluck(newPotLuck);
-        Assertions.assertNotEquals("", newPotLuck.getId());
-    }
-
-    @Test
-    @Order(8)
-    void firstValidationTimeTest()throws InvalidTimeException{
-
-        Potluck newPotLuck = new Potluck("Second", System.currentTimeMillis() + 10000L,
-                "username",true);
-        Assertions.assertThrows(InvalidTimeException.class,
-                () -> potluckService.schedulePotluck(newPotLuck),
-                "The time you wish to schedule the potluck is within an hour of a currently scheduled Potluck.");
-    }
-
-    @Test
-    @Order(9)
-    void secondValidationTimeTest()throws InvalidTimeException{
-
-        Potluck newPotLuck = new Potluck("Second", 1000L, "username",true);
-        Assertions.assertThrows(InvalidTimeException.class,
-                () -> potluckService.schedulePotluck(newPotLuck),
-                "The time you wish to schedule the potluck has already passed.");
-    }
-
-    @Test
-    @Order(10)
-    void cancelNegativeTest() {
-        Assertions.assertTrue(potluckService.cancelPotluck(testPotluck1));
-    }
-
-    @Test
-    @Order(11)
-    void noPotluckIdTest()throws NullPointerException{
-        Assertions.assertThrows(NullPointerException.class,
-               () -> potluckService.getPotluckById(testPotluck.getId()),
-                "Potluck id was not found: " + testPotluck.getId());
-    }
-
 }
