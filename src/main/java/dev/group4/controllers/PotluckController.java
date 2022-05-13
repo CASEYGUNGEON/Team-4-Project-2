a664bcf0c5f9ffa8d8482a28a6e4893eb8dff140
dmbrumagin <dbrumagin@gmail.com>
2022-05-12 23:22:14 -0400

    potluck controller bare bones

--- src/main/java/dev/group4/controllers/PotluckController.java
+++ src/main/java/dev/group4/controllers/PotluckController.java
@@ -34,23 +34,26 @@ public class PotluckController {
     }
 
     @Secured
-    @PatchMapping("user/{user_id}/potluck/{potluck_id}")
-    public Potluck changePotluck(@RequestBody Potluck potluck,@PathVariable String potluck_id,@PathVariable String user_id){
+    @PatchMapping("potluck/{potluck_id}")
+    public Potluck changePotluck(@RequestBody Potluck potluck,@PathVariable String potluck_id){
         potluck.setId(potluck_id);
-        potluck.setCreatorId(user_id);
         try {
             return potluckService.changePotluckTime(potluck);
         } catch (InvalidTimeException e) {
             throw new RuntimeException(e);
         }
     }
+
+    @Secured
+    @DeleteMapping("/potlucks/{potluck_id}")
+    public void deletePotluck(@PathVariable String potluck_id){
+        potluckService.cancelPotluck(potluck_id);
+    }
+
+    @Secured
+    @GetMapping("/potlucks/{potluck_id}")
+    public Potluck shareLink(@PathVariable String potluck_id) {
+        return potluckService.getPotluckById(potluck_id);
+    }
+
 }
-/*As a registered User I can edit my potluck date/time
-PATCH {host}/potluck/{id}    @secured
-As a registered User I can delete my potlucks
-DELETE {host}/potlucks/{potluck_id}   @secured
-As a registered User I can create a sharable link that goes directly to my potluck
-GET {host}/potlucks/{potluck_id} (has a button) @secured
-As a registered user I can create private potlucks that require a shareable link to be seen
-POST  {host}/potlucks/{potluck_id}?=visibility @secured
-*/
