927b78c79cd55a20dbe050389709bea51db04fef
CASEY GUNGEON <bound2theground@gmail.com>
2022-05-14 15:52:01 -0700

    basic, buggy fulfillment of items

--- website/src/items.js
+++ website/src/items.js
@@ -23,28 +23,34 @@ export default function Items(props) {
     async function fulfill(item) {
         if(loggedIn)
             item.supplier = username;
-        else {
-            if(guestName != "") {
-                const req = await fetch(host + "postlucks/" + chosenPotluck + "/items",
-                    { 
-                        method: "PATCH",
-                        headers: { "Content-type": "application/json" },
-                        body: JSON.stringify({item})
-                    });
-                const body = await req.json();
-            }
-            else {
-
-            }
+        else if(guestName != "") {
+            item.supplier = guestName;
+        } else {
+            return null;
         }
+
+        const req = await fetch(host + "/potlucks/" + chosenPotluck + "/items",
+            { 
+                method: "PATCH",
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify(
+                    {
+                        itemId: item.id,
+                        description: item.description,
+                        potluckId: chosenPotluck,
+                        status : "fulfilled",
+                        supplier: item.supplier
+                }),
+            });
+        const body = await req.json();
     }
 
     const listElement = itemList.map((n) => (
         <tr key={n.id}>
             <td>{n.description}</td>
             <td>{n.status.toString()}</td>
-            <td>{n.supplier}</td>
-            <td><button onClick={() => fulfill(n)} /></td>
+            <td>{n.supplier!=null?n.supplier:"N/A"}</td>
+            <td><button onClick={() => fulfill(n)}>Fulfill</button></td>
         </tr>
     ))
 
@@ -57,6 +63,7 @@ export default function Items(props) {
                     </tr>
                     {listElement}
                 </thead>
-            </table>
+            </table><br/>
+            <input name="name" type="text" placeholder="input name to fulfill" onChange={(e) => setGuestName(e.target.value)} />
         </>);
 }
