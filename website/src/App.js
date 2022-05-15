b1dc7c37afb84429cd42b94d937933dd7bbc8e1f
dmbrumagin <dbrumagin@gmail.com>
2022-05-15 17:17:45 -0400

    user creation

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState, useEffect } from 'react'
+import UserCreator from './user'
 import UserHandler from './UserHandler'
 import Potlucks from './potlucks'
 import Items from './items'
@@ -16,16 +17,13 @@ function App() {
   const[pageDisplay,setPageDisplay] = useState("potluckList");
   const [password, setPassword] = useState([]);
 
-  function createNewUser(){
-    console.log("hello");
-  }
   useEffect(() => { 
     if(sessionStorage.getItem("auth"))
       setUsername(atob(JSON.parse(sessionStorage.getItem("auth")).authorization).split(":")[0]);
 }, []);
 
 
-  const jsx = [<div id='header'><div id='brand'><img id='logo' src={logo}/></div><div id='saying'><h1>Gather</h1><p id='sub-saying'>Food brings us together</p></div><div id='user'><UserHandler className="userHandler" chosenPotluck = {chosenPotluck} setPageDisplay={setPageDisplay} password={password} setPassword = {setPassword} username={username} setUsername={setUsername} loggedIn={loggedIn} setLoggedIn={setLoggedIn} createUser={createNewUser} host={host} key="userHandler" /></div></div>];
+  const jsx = [<div id='header'><div id='brand'><img id='logo' src={logo}/></div><div id='saying'><h1>Gather</h1><p id='sub-saying'>Food brings us together</p></div><div id='user'><UserHandler className="userHandler" chosenPotluck = {chosenPotluck} setPageDisplay={setPageDisplay} password={password} setPassword = {setPassword} username={username} setUsername={setUsername} loggedIn={loggedIn} setLoggedIn={setLoggedIn} host={host} key="userHandler" /></div></div>];
 
 
   if(pageDisplay === "index") {
@@ -36,6 +34,9 @@ function App() {
   }
   else if(pageDisplay === "items") {
     jsx.push(<div id='display'><Items chosenPotluck={chosenPotluck} setPageDisplay={setPageDisplay} username={username} loggedIn={loggedIn} host={host} key="items" /></div>)
+  }
+  else if(pageDisplay === "user"){
+    jsx.push(<div id='display'><UserCreator chosenPotluck={chosenPotluck} setPageDisplay={setPageDisplay} username={username} setUsername = {setUsername} password = {password} setPassword = {setPassword}   host={host} key="user"  /></div>)
   }
 
   return(<div className='main'><div id='center'>{jsx}</div></div>);
--- website/src/UserHandler.js
+++ website/src/UserHandler.js
@@ -8,7 +8,7 @@ export default function UserHandler(props) {
     const setPassword = props.setPassword;
     const loggedIn = props.loggedIn;
     const setLoggedIn = props.setLoggedIn;
-    const createNewUser = props.createNewUser;
+    //const createNewUser = props.createNewUser;
     const host = props.host;
     const setPage = props.setPageDisplay;
     const page = props.chosenPotluck;
@@ -39,20 +39,23 @@ export default function UserHandler(props) {
         setLoggedIn(false);
         window.location.reload(false);
     }
+    function createNewUser(){
+        setPage('user');
+    }
 
 
 
     if(!sessionStorage.getItem("auth")) {
         return(
             <div className="right">
-            <form>
+            
             <label htmlFor="username">Username </label>
             <input  type="text" name="username" onChange={(e) => setUsername(e.target.value)} required placeholder="username" /><br />
             <label htmlFor="password">Password </label>
             <input  type="password" name="password" onChange={(e) => setPassword(e.target.value)} required placeholder="password" /><br/>
             <button onClick={() => createNewUser()}>Create User</button>
             <button className="rightbutton" onClick={(e) =>{e.preventDefault(); sendLogin()}}>Log In</button>
-            </form>
+           
             </div>
         );
     }
