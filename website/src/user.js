feb54c11949bd04a2aedd9b2e28883363d0e896b
dmbrumagin <dbrumagin@gmail.com>
2022-05-15 17:29:43 -0400

    user creation

--- website/src/user.js
+++ website/src/user.js
@@ -1,47 +1,50 @@
 import {useState} from 'react';
-import '../App.css';
+import './App.css';
 
-export default function user(){
+export default function user(props){
+    const page = props.setPageDisplay;
+    const username = props.username;
+    const setUsername = props.setUsername;
+    const password = props.password;
+    const setPassword = props.setPassword;
+    const host = props.host;
 
-    const [username, setUsername] = useState([]);
-    const [password, setPassword] = useState([]);
+    async function createLogin(){
 
-    function usernameUpdate(event){
-        setUsername(event.target.value);
-    }
 
-    function passwordUpdate(event){
-        setPassword(event.target.value);
-    }
+        const user = {username,password};
+        console.log(user);
 
-    async function sendLogin(){
-        const a =sessionStorage.getItem("auth");
-        const response = await fetch("https://52c8-67-253-63-98.ngrok.io/users",{
-            method:"GET", 
+        const response = await fetch(`${host}/users`,{
+            body:JSON.stringify(user),
+            method:"POST",
             headers:{
-                "Authorization":btoa(`${username}:${password}`)
-            }
-        });        
-
-        const name = btoa(`${username}:${password}`);
-        console.log(name);
-        const auth = await response.json();
-        sessionStorage.setItem("auth", JSON.stringify(auth));
+                "Content-Type":"application/json"
+            }     
+        });
+        if(response.status === 200){
+            const body = await response.json();       
+            alert(`New user registered${body.username}`)
+            window.location.reload(false);
+        }else{
+            alert("FAILED TO CREATE A User");
+        }
     }
 
-    return(<>
-
-        <label htmlFor="username"> Username</label>
-        <input onChange={usernameUpdate} name="username" type="text" placeholder="username"/>
+    return(<><div id='create'>
+        <label>Create a User:<br/></label>
+    <label htmlFor="username"> Username</label>
+    <input  type="text" name="username" onChange={(e) => setUsername(e.target.value)} required placeholder="username" />
 
         <label htmlFor="password">Password</label>
-        <input onChange={passwordUpdate}  pattern="(?=.*\d)(?=.*[A-Z]).{6,}" type="password" id="password" name="password" required placeholder="password"></input>
-        <div class="requirements">You must have the following:
+        <input pattern="(?=.*\d)(?=.*[A-Z]).{6,}"  type="text" name="password" id='password' onChange={(e) => setPassword(e.target.value)} required placeholder="password" />
+       <div class="requirements">
+            <label>You must have the following:</label>
         <ul><li>An Uppercase Letter</li>
             <li>A Number</li>
-            <li>A Special Character</li></ul></div>
-
-        <button onClick={sendLogin}>Login</button>
+            <li>A Special Character</li></ul></div></div>
+            <button onClick={createLogin}>Submit</button>
+    
     </>)
     
 }
