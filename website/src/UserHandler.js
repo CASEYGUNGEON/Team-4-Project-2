ec8da3ab9e920b456d6ed9b03fb356b7a1bdf38f
dmbrumagin <dbrumagin@gmail.com>
2022-05-15 15:47:10 -0400

    login/logout and username persistence

--- website/src/UserHandler.js
+++ website/src/UserHandler.js
@@ -1,30 +1,64 @@
 import './App.css';
-import {useState } from 'react';
+import {useEffect, useState } from 'react';
 
 export default function UserHandler(props) {
     const username = props.username;
     const setUsername = props.setUsername;
+    const password = props.password;
+    const setPassword = props.setPassword;
     const loggedIn = props.loggedIn;
     const setLoggedIn = props.setLoggedIn;
     const createNewUser = props.createNewUser;
-    let password = '';
+    const host = props.host;
+    const setPage = props.setPageDisplay;
+    const page = props.chosenPotluck;
+
+   
 
 
-    if(!loggedIn) {
+    async function sendLogin(){
+        console.log("sending login");
+        const a =sessionStorage.getItem("auth");
+        const response = await fetch(host+"/users",{
+            method:"GET", 
+            headers:{
+                "Authorization":btoa(`${username}:${password}`)
+            }
+        });        
+        setLoggedIn(true);
+
+        const name = btoa(`${username}:${password}`);
+        console.log(name);
+        const auth = await response.json();
+        sessionStorage.setItem("auth", JSON.stringify(auth));
+        window.location.reload(false);
+    }
+
+    function logout(){
+        sessionStorage.removeItem("auth");
+        setLoggedIn(false);
+        window.location.reload(false);
+    }
+
+
+
+    if(!sessionStorage.getItem("auth")) {
         return(
             <div className="right">
+            <form>
             <label htmlFor="username">Username </label>
-            <input type="text" name="username" onChange={(e) => setUsername(e.target.value)} placeholder="username" /><br />
+            <input  type="text" name="username" onChange={(e) => setUsername(e.target.value)} required placeholder="username" /><br />
             <label htmlFor="password">Password </label>
-            <input type="text" name="password" onChange={(e) => password = e.target.value} placeholder="password" /><br/>
+            <input  type="password" name="password" onChange={(e) => setPassword(e.target.value)} required placeholder="password" /><br/>
             <button onClick={() => createNewUser()}>Create User</button>
-            <button className="rightbutton" onClick={() => setLoggedIn(true)}>Log In</button>
+            <button className="rightbutton" onClick={(e) =>{e.preventDefault(); sendLogin()}}>Log In</button>
+            </form>
             </div>
         );
     }
-    else
+    else{
+        console.log("username:"+ username +" logout");
         return(<>
-            <div>Welcome, {username} <button onClick={() => setLoggedIn(false)}>Log Out</button></div>
-            
-        </>);
+            <div>Welcome, {username} <button onClick={() => logout()}>Log Out</button></div>  
+        </>);}
 }
